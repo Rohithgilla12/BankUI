e5a982117dabcf9978abb82ffcf64b47431f8032
Rohithgilla12 <gillarohith1@gmail.com>
2020-07-17 16:29:26 +0530

    Initial Commit ✌🏻

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:bank_ui/theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: BankTheme.black,
+      appBar: buildAppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(
+            right: 16.0,
+          ),
+          child: IconButton(
+            icon: Icon(
+              Icons.notifications_none,
+              size: 32,
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ],
+      title: Row(
+        children: [
+          CircleAvatar(
+            radius: 24,
+            backgroundImage: NetworkImage(
+              "https://lh3.googleusercontent.com/a-/AOh14GiZ8uDLrYJ6qZ51x_LUT2j5LFmhYL-LP6Yn-1WTdao=s88-c-k-c0x00ffffff-no-rj-mo",
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 16.0,
+            ),
+            child: Text(
+              'Hi, Gilla!',
+              style: TextStyle(
+                fontSize: 24,
+                fontFamily: 'Avenir',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BankTheme {
+  static const Color black = Color(0xff040417);
+
+  static const Color lightBlue = Color(0xffa4a7fa);
+
+  static const Color palePurple = Color(0xffccd0fd);
+
+  static const Color pink = Color(0xffea5eb8);
+
+  static const Color violet = Color(0xff8f619d);
+
+  static const Color primary = Color(0xff3e2f56);
+
+  static const Color orange = Color(0xfff5654e);
+}
