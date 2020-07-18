dab28316f6ffaead03a017ad720892537c325bcf
Rohithgilla12 <gillarohith1@gmail.com>
2020-07-18 20:47:59 +0530

    Cards UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bank_ui/theme.dart';
 import 'package:bank_ui/widgets/card_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_sparkline/flutter_sparkline.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Nunito',
       ),
       home: MyHomePage(),
     );
@@ -42,7 +42,13 @@ class _MyHomePageState extends State<MyHomePage> {
     );
     var data = [0.0, 0.3, 0.2, 0.4, 0.3, 0.2, 0.3];
     var data2 = [0.0, 0.1, 0.3, 0.2, 0.0, 0.1, 0.2];
+    var cardHeadingStyle = TextStyle(
+      color: Colors.white,
+      fontWeight: FontWeight.w700,
+      fontSize: 20,
+    );
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       backgroundColor: BankTheme.black,
       appBar: buildAppBar(),
       body: Column(
@@ -57,19 +63,17 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Container(
               height: 400,
               child: Stack(
-                // alignment: Alignment.bottomCenter,
-                // overflow: Overflow.clip,
                 children: [
                   Container(
-                    height: 120,
+                    height: 140,
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
                         colors: [
                           BankTheme.orange,
-                          Colors.orange[100],
+                          Color(0xffFEC18A),
                         ],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
                       ),
                       borderRadius: BorderRadius.only(
                         topLeft: Radius.circular((30)),
@@ -78,6 +82,23 @@ class _MyHomePageState extends State<MyHomePage> {
                       border: Border.all(
                         color: BankTheme.black,
                         width: 4,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(32.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Salary card',
+                            style: cardHeadingStyle,
+                          ),
+                          Text(
+                            '\$ 1,112',
+                            style: cardHeadingStyle,
+                          ),
+                        ],
                       ),
                     ),
                   ),
@@ -102,8 +123,25 @@ class _MyHomePageState extends State<MyHomePage> {
                             // BankTheme.palePurple,
                             Color(0xffE0ABF2)
                           ],
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(32.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Private card',
+                              style: cardHeadingStyle,
+                            ),
+                            Text(
+                              '\$ 5,437',
+                              style: cardHeadingStyle,
+                            ),
+                          ],
                         ),
                       ),
                     ),
@@ -113,7 +151,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     left: 0.0,
                     right: 0.0,
                     child: Container(
-                      height: 220,
+                      height: 200,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.all(
                           Radius.circular((30)),
@@ -131,6 +169,76 @@ class _MyHomePageState extends State<MyHomePage> {
                           begin: Alignment.topLeft,
                           end: Alignment.bottomRight,
                         ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        // crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 32,
+                              right: 32,
+                              top: 16,
+                              bottom: 16,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Family card',
+                                      style: cardHeadingStyle,
+                                    ),
+                                    Text(
+                                      '**** 2616',
+                                      style: TextStyle(
+                                        color: Colors.white70,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Text(
+                                  '\$ 15,269',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 24,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              left: 32,
+                              right: 32,
+                              top: 16,
+                              bottom: 0,
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                CircleAvatar(
+                                  radius: 20,
+                                  backgroundImage: NetworkImage(
+                                    "https://lh3.googleusercontent.com/a-/AOh14GiZ8uDLrYJ6qZ51x_LUT2j5LFmhYL-LP6Yn-1WTdao=s88-c-k-c0x00ffffff-no-rj-mo",
+                                  ),
+                                ),
+                                Container(
+                                  height: 42,
+                                  child: Image.network(
+                                    'https://www.montyhalls.co.uk/wp-content/uploads/2014/11/logo-visa.png',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
